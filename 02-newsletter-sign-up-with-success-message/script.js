8120a34d57dc3e4cc55015d6e38b9974843fd0fb
Edwin Kofler <edwin@kofler.dev>
2023-07-26 08:21:38 -0700

    02: Write initial implementation

---
+++ 02-newsletter-sign-up-with-success-message/script.js
@@ -0,0 +1,22 @@
+const subscribeButtonEl = document.querySelector('button.newsletter-subscribe')
+const dismissButtonEl = document.querySelector('button.dismiss-message')
+
+const cardStayUpdated = document.querySelector('.card-stay-updated')
+const cardThanksForSubscribing = document.querySelector(
+	'.card-thanks-for-subscribing',
+)
+
+subscribeButtonEl.addEventListener('click', (ev) => {
+	cardStayUpdated.attributeStyleMap.set('display', new CSSKeywordValue('none'))
+	cardThanksForSubscribing.attributeStyleMap.set(
+		'display',
+		new CSSKeywordValue('block'),
+	)
+})
+dismissButtonEl.addEventListener('click', (ev) => {
+	cardStayUpdated.attributeStyleMap.set('display', new CSSKeywordValue('flex'))
+	cardThanksForSubscribing.attributeStyleMap.set(
+		'display',
+		new CSSKeywordValue('none'),
+	)
+})
